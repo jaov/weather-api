d930ec55d7feb22c6cee3e7710c08708fe50268f
OJ <me@oveja.dev>
2025-12-17 15:42:09 -0400

    Movida logica a Controller.

---
+++ app/Http/Controllers/ClimaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ClimaController extends Controller
+{
+    //
+    public function forecast(string $ciudad) {
+        $appid='';
+        $response = Http::get('https://api.openweathermap.org/geo/1.0/direct?q=' . urlencode($ciudad) . '&appid=' . $appid . '&limit=1');
+        $lat = $response[0]['lat'];
+        $lon = $response[0]['lon'];
+
+        $forecast = Http::get('https://api.openweathermap.org/data/3.0/onecall?lon='. urlencode($lon). '&lat='. urlencode($lat) . '&appid=' . $appid . '&units=metric' );
+        return array_slice(($forecast['daily']), 0,3);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,17 @@
 <?php
 
-use App\Http\Resources\ClimateResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClimaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('clima/{ciudad}', function ($ciudad) {
-    $appid='';
-    $response = Http::get('https://api.openweathermap.org/geo/1.0/direct?q=' . urlencode($ciudad) . '&appid=' . $appid . '&limit=1');
-    $lat = $response[0]['lat'];
-    $lon = $response[0]['lon'];
-
-    $forecast = Http::get('https://api.openweathermap.org/data/3.0/onecall?lon='. urlencode($lon). '&lat='. urlencode($lat) . '&appid=' . $appid . '&units=metric' );
-    return array_slice(($forecast['daily']), 0,3);
+Route::get('home', function () {
+    return view('welcome');
 });
+
+Route::get('dashboard', function () { return view('dashboard');});
+
+Route::get('clima/{ciudad}', [ClimaController::class, 'forecast']);
