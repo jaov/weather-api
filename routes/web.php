8702d0e51edf4354de97d4aac22c685eea8c5991
jesuso <j.ochoa@norteconecta.net>
2025-12-23 16:36:54 -0400

    cambiada ruta para usar controlador

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClimaController;
+use App\Http\Controllers\TokenController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,10 +20,6 @@ Route::get('clima', function () { return view('clima');});
 
 Route::post('clima', function(Request $request) { $ciudad = $request->input('ciudad'); return ClimaController::forecast($ciudad);});
 
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
-});
+Route::post('/tokens/create', [TokenController::class, 'web_create']);
 
 Route::get('/user/token', function (Request $request) { return view('auth.token');})->middleware('auth:sanctum');
