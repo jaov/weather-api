3ff62377a12625f76d9dd21223eec7be0f5d27b4
jesuso <j.ochoa@norteconecta.net>
2025-12-23 16:37:07 -0400

    agregadas rutas a api.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClimaController;
+use App\Http\Controllers\TokenController;
+use App\Models\User;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('clima/{ciudad}', [ClimaController::class, 'forecast'])->middleware('auth:sanctum');
+
+Route::post('/tokens/create', [TokenController::class, 'create']);
+
+
